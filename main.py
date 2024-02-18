2862ce4c23bb7d6345c5758e0f44aad9a296a687
Brena Fernanda Vicente de Souza <42597791+breenafernanda@users.noreply.github.com>
2024-02-18 18:09:55 -0300

    Import Keys

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.keys import Keys
 
 async def aguardar_elemento(driver, tempo, tipo, code):
     try:
